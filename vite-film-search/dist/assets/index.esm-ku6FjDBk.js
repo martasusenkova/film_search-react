91ec61f98e65d0daa695b5120b701695e905fd6d
martasusenkova <susenkova582@gmail.com>
2025-11-26 18:12:25 +0400

    fix config

--- vite-film-search/dist/assets/index.esm-ku6FjDBk.js
+++ vite-film-search/dist/assets/index.esm-ku6FjDBk.js
@@ -1,4 +1,4 @@
-import{r as A,_ as v,C as S,a as k,E as Y,o as Ce,F as J,g as Ee,b as y,d as ke,i as X,c as Q,e as Z,v as ee,L as Pe,f as U}from"./index-BidK8CQl.js";const te="@firebase/installations",N="0.6.19";/**
+import{r as A,_ as v,C as S,a as k,E as Y,o as Ce,F as J,g as Ee,b as y,d as ke,i as X,c as Q,e as Z,v as ee,L as Pe,f as U}from"./index-DHokBsry.js";const te="@firebase/installations",N="0.6.19";/**
  * @license
  * Copyright 2019 Google LLC
  *
